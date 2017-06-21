7345fbccc447448550423c6d7fe107ae01cb355c
sprayzomb <oguzsarigul@protonmail.com>
2017-06-21 18:07:03 +0300

    Merge branch 'master' of C:\Users\oguz.sarigul\PycharmProjects\Relief\Numericon with conflicts.

--- xzc.py
+++ xzc.py
@@ -1,17 +1,20 @@
-from tkinter import *
+import tkinter
 from tkinter import ttk
 import pandas as pd
+import ctypes
 
-
-class Program(Frame):
+class Program(ttk.Frame):
     accumulation = 1.1
-    Kd = 0.97
+    Kd = 0.975
     Kc = 1
 
     def __init__(self, parent, *args, **kwargs):
-        Frame.__init__(self, parent, *args, **kwargs)
+        ttk.Frame.__init__(self, parent, *args, **kwargs)
         self.root = parent
         self.init_gui()
+
+    def Mbox(self, title, text, style):
+        return ctypes.windll.user32.MessageBoxW(0,text,title,style)
 
     def on_quit(self):
         self.root.destroy()
@@ -28,9 +31,12 @@ class Program(Frame):
     def rupture(self):
         if (self.kdcoeffvar.get() ==1):
             self.Kd = 0.62
+            self.combinationcorrection.configure(state='disabled')
+            self.combinationcorrection.state(['!selected'])
         else:
             self.Kd = 0.975
-            #self.combinationcorrection.state(state=DISABLED)
+            self.combinationcorrection.configure(state='enabled')
+            self.combinationcorrection.state(['!selected'])
 
     #for combination correction factor for installation with a rupture
     #  disk upstream of the pressure relief valve
@@ -41,11 +47,20 @@ class Program(Frame):
             self.Kc = 1
 
     def calculate(self):
-        # Constant Values
+        try:
+            raw_flow = float(self.flow_entry.get())
+        except ValueError:
+            self.Mbox("Error", "Please enter a valid flowrate.", 1)
 
-        raw_flow = int(self.flow_entry.get())
-        raw_pressure = int(self.pressure_entry.get())
-        raw_temperature = int(self.temperature_entry.get())
+        try:
+            raw_pressure = float(self.pressure_entry.get())
+        except ValueError:
+            self.Mbox("Error", "Please enter a valid pressure.", 1)
+
+        try:
+            raw_temperature =float(self.temperature_entry.get())
+        except ValueError:
+            self.Mbox("Error", "Please enter a valid temperature.", 1)
 
         unit_flow = self.unit_index_flow.get()
         unit_pressure = self.unit_index_pressure.get()
@@ -53,9 +68,9 @@ class Program(Frame):
 
         def flow_unit_conversion(unit_flow, raw_flow):
             if unit_flow == "kg/h":
-                return raw_flow * 2.20462262185
+                return raw_flow * 2.2046
             elif unit_flow == "kg/s":
-                return raw_flow *2.20462262185 /3600
+                return raw_flow *2.2046 /3600
             elif unit_flow == "lb/h":
                 return raw_flow
             elif unit_flow == "lb/s":
@@ -81,7 +96,7 @@ class Program(Frame):
         converted_temperature = temperature_unit_conversion(unit_temperature, raw_temperature)
         converted_pressure = pressure_unit_conversion(unit_pressure, raw_pressure)
 
-#data pulling from table starts here
+    #data pulling from table starts here
         def roundup_temp(converted_temperature):
             return table_df.columns[table_df.columns >= converted_temperature].min()
 
@@ -96,7 +111,7 @@ class Program(Frame):
         table_df.index.name = None
         table_df.columns = table_df.columns.astype(int)
         self.Ksh=pull_data(converted_pressure, converted_temperature)
-#data pulling ends here
+    #data pulling ends here
 
         #P1= Relieving Pressure
         self.P1=converted_pressure*self.accumulation+14.7
@@ -106,58 +121,48 @@ class Program(Frame):
         elif self.P1 <= 3200 and self.P1 > 1500:
             self.Kn=(0.1906*self.P1-1000)/(0.2292*self.P1-1061)
 
-        self.Kb1= -2.428*(14.7/converted_pressure)**2+(14.7/converted_pressure)*0.372+1.108
-        self.Kb2 = 1
+        self.Kb= -2.428*(14.7/converted_pressure)**2+(14.7/converted_pressure)*0.372+1.108
+        self.Kb = 1
 
-        A=converted_flow/(51.5*self.P1*self.Kd*self.Kb1*self.Kc*self.Kn*self.Ksh)
-        A_rounded=round(A,2)
+        A=converted_flow/(51.5*self.P1*self.Kd*self.Kb*self.Kc*self.Kn*self.Ksh)
+        A_rounded=round(A, 2)
 
-        self.answer_label['text'] = A_rounded
+        self.answer_label['text'] = A
 
-        #print(converted_flow)
-        #print(self.unit_index_flow)
-        #print(unit_flow)
-        #print(converted_pressure)
-        #print(self.unit_index_pressure)
-        #print(unit_pressure)
-        #print(converted_temperature)
-        #print(self.unit_index_temperature)
-        #print(unit_temperature)
-        #print(self.Kb1)
         print(self.Kd)
 
     def init_gui(self):
         self.root.title('Sizing Pressure-Relieving Devices (API 520)')
         self.grid(column=0, row=0, sticky='nsew')
 
-        self.firevar = IntVar()
-        self.firecase = Checkbutton(self, text='Fire Case', variable=self.firevar, command=self.fire)
+        self.firevar = tkinter.IntVar()
+        self.firecase = ttk.Checkbutton(self, text='Fire Case', variable=self.firevar, command=self.fire)
         self.firecase.grid(column=0, row=11, sticky="W")
 
-        self.instvar = IntVar()
-        self.firecase = Checkbutton(self, text='Valve Installation mode', variable=self.instvar, command=self.fire)
+        self.instvar = tkinter.IntVar()
+        self.firecase = ttk.Checkbutton(self, text='Valve Installation mode', variable=self.instvar, command=self.fire)
         self.firecase.grid(column=1, row=11, sticky="W")
 
 #getting effective coefficient of discharge input starts here
-        self.kdcoeffvar = IntVar()
-        self.effectivecoefdisc = Checkbutton(self, text='A pressure relief valve is not installed and '
+        self.kdcoeffvar = tkinter.IntVar()
+        self.effectivecoefdisc = ttk.Checkbutton(self, text='A pressure relief valve is not installed and '
                                                           'sizing is for a rupture disk',
                                                variable=self.kdcoeffvar, command=self.rupture,
                                              wraplength=300, justify=LEFT, anchor="e")
         self.effectivecoefdisc.grid(column=0, row=12, columnspan=4, rowspan=2, sticky="W")
 #getting effective coefficient of discharge input ends here
 
-        self.kccoeffvar = IntVar()
-        self.combinationcorrection = Checkbutton(self, text='A rupture disk is installed in combination with'
+        self.kccoeffvar = tkinter.IntVar()
+        self.combinationcorrection = ttk.Checkbutton(self, text='A rupture disk is installed in combination with'
                                                           ' a pressure relief valve',
                                                variable=self.kccoeffvar, command=self.ruptureinst,
                                                  wraplength=300, justify=LEFT, anchor="e")
         self.combinationcorrection.grid(column=0, row=14, sticky="W", columnspan=4, rowspan=2)
 
 #GETTING FLOW AND UNIT STARTS HERE
-        self.flow_entry = Entry(self, width=15)
+        self.flow_entry = ttk.Entry(self, width=10)
         self.flow_entry.grid(column=1, row=2, sticky="W")
-        self.unit_index_flow = StringVar()
+        self.unit_index_flow = tkinter.StringVar()
         self.box = ttk.Combobox(self, textvariable=self.unit_index_flow,
                                 state='readonly', width=8)
         self.box['values'] = ("kg/h", "kg/s", "lb/h", "lb/s")
@@ -167,9 +172,9 @@ class Program(Frame):
 # GETTING FLOW AND UNIT STARTS HERE
 
 # GETTING PRESSURE AND UNIT STARTS HERE
-        self.pressure_entry = Entry(self, width=15)
+        self.pressure_entry = ttk.Entry(self, width=10)
         self.pressure_entry.grid(column=1, row=3, sticky="W")
-        self.unit_index_pressure = StringVar()
+        self.unit_index_pressure = tkinter.StringVar()
         self.box = ttk.Combobox(self, textvariable=self.unit_index_pressure,
                                 state='readonly', width=8)
         self.box['values'] = ("psig", "barg", "kPag")
@@ -178,9 +183,9 @@ class Program(Frame):
 # GETTING PRESSURE AND UNIT ENDS HERE
 
 # GETTING TEMPERATURE AND UNIT STARTS HERE
-        self.temperature_entry = Entry(self, width=15)
+        self.temperature_entry = ttk.Entry(self, width=10)
         self.temperature_entry.grid(column=1, row=4, sticky="W")
-        self.unit_index_temperature = StringVar()
+        self.unit_index_temperature = tkinter.StringVar()
         self.box = ttk.Combobox(self, textvariable=self.unit_index_temperature,
                                 state="readonly", width=8)
         self.box["values"] = ("F", "K", "°C")
@@ -188,16 +193,16 @@ class Program(Frame):
         self.box.grid(column=2, row=4)
 # GETTING TEMPERATURE AND UNIT ENDS HERE
 
-        self.calc_button = Button(self, text='Calculate', command=self.calculate, width=10)
+        self.calc_button = ttk.Button(self, text='Calculate', command=self.calculate)
         self.calc_button.grid(column=2, row=5, columnspan=4, sticky="E")
 
-        self.quit_button = Button(self, text='Quit', command=self.on_quit, width=10)
-        self.quit_button.grid(column=2, row=16, columnspan=4, sticky="E")
+        self.quit_button = ttk.Button(self, text='Quit', command=self.on_quit)
+        self.quit_button.grid(column=2, row=13, columnspan=4, sticky="E")
 
-        self.answer_frame = LabelFrame(self, text='Answer', height=100)
+        self.answer_frame = ttk.LabelFrame(self, text='Answer', height=100)
         self.answer_frame.grid(column=0, row=6, columnspan=4, sticky='nesw')
 
-        self.answer_label = Label(self.answer_frame, text='')
+        self.answer_label = ttk.Label(self.answer_frame, text='')
         self.answer_label.grid(column=0, row=0)
 
         #constant labels
